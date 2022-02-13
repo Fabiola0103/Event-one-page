25e4ba2d8637cbfa8002233eb7a126444eb49c72
Fabiola <fabiola.az.0103@gmail.com>
2022-02-13 16:57:40 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const minhamodal = new bootstrap.Modal(document.getElementById('minhamodal1'), {
+    keyboard: false
+  })
+const botao1 = document.getElementById("botaomodal1")
+
+
+botao1.addEventListener("click", function(){
+    console.log("clicou no botao");
+    minhamodal.show();
+});
+
+const botao2 = document.getElementById("botaomodal2")
+
+
+botao2.addEventListener("click", function(){
+    console.log("clicou no botao");
+    minhamodal.show();
+});
+
+const botao3 = document.getElementById("botaomodal3")
+
+
+botao3.addEventListener("click", function(){
+    console.log("clicou no botao");
+    minhamodal.show();
+});
+
+const botao4 = document.getElementById("botaomodal4")
+
+
+botao4.addEventListener("click", function(){
+    console.log("clicou no botao");
+    minhamodal.show();
+});
+
+const botao5 = document.getElementById("botaomodal5")
+
+
+botao5.addEventListener("click", function(){
+    console.log("clicou no botao");
+    minhamodal.show();
+});
